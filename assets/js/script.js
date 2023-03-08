5cb980f3914253e3eb562772c533431200567ec9
Igor <soriedemrogi@gmail.com>
2023-03-08 18:08:59 -0300

    identação do arquivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-const counters = document.querySelectorAll('strong')
+const counters = document.querySelectorAll("strong");
 
-
-counters.forEach(counter => {
-  counter.textContent = '0'
+counters.forEach((counter) => {
+  counter.textContent = "0";
 
   const updateCounter = () => {
-    const target = Number(counter.getAttribute('data-target'))
-    const content = Number(counter.textContent)
-    const increment = target/200
+    const target = Number(counter.getAttribute("data-target"));
+    const content = Number(counter.textContent);
+    const increment = target / 200;
 
-    if(content < target) {
-      counter.textContent = `${Math.ceil(content+increment)}`
-      setTimeout(updateCounter, 1)
+    if (content < target) {
+      counter.textContent = `${Math.ceil(content + increment)}`;
+      setTimeout(updateCounter, 1);
     }
-  }
-  
-  updateCounter()
+  };
 
-})
+  updateCounter();
+});
